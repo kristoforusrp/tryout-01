802679978c9aa844ffde2a7fe7e480492bf7ac63
kristoforusrp <kristoforusrp@gmail.com>
2017-04-01 20:46:12 +0700

    yield - user can determine the folder path

--- yield/index.js
+++ yield/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
 function *readDirFile() {
-  yield fs.readdir('/etc/nginx', (err, list) => {
+  process.stdout.write(`The ${process.argv[2]} folder contains: \n`) 
+  yield fs.readdir(process.argv[2], (err, list) => {
     if (err) throw err;
     list.map((item, idx) => process.stdout.write(`${idx + 1}: ${item}\n`));
   });
